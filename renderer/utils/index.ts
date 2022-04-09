6fabe55f7cf814b68d8ca2bf07bce5298751e8f1
Lisianthus-A <lisianthus.zeng@gmail.com>
2022-04-09 17:03:50 +0800

    feat: add util getDuration

--- renderer/utils/index.ts
+++ renderer/utils/index.ts
@@ -10,3 +10,25 @@ export const getFileName = (path: string) => {
         return tempPath.slice(0, tempPath.lastIndexOf("."));
     }
 };
+
+export const getDuration = async (path: string) => {
+    const buffer = await window.electronAPI.readFile(path);
+    if (!buffer) {
+        return 0;
+    }
+    const blob = new Blob([buffer], { type: "audio/wav" });
+    const src = window.URL.createObjectURL(blob);
+    const result = await new Promise<number>((resolve) => {
+        const audio = new Audio();
+        audio.onloadedmetadata = () => {
+            resolve(audio.duration);
+        };
+        audio.onerror = () => {
+            resolve(0);
+        };
+        audio.src = src;
+    });
+
+    window.URL.revokeObjectURL(src);
+    return result;
+}
